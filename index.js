08b989383d5a19ed12bcd9d3b7129aad8fdbb42d
ranyerino1 <jetacuma3@misena.edu.co>
2022-12-04 13:53:09 -0500

    asd

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const conexionBD = require('./db');
 const app = express()
+const PORT = process.env.PORT || 3000
 conexionBD()
 app.use(express.static(__dirname + '/public'));
 app.set('view engine', 'ejs')
@@ -9,6 +10,6 @@ app.use(express.json())
 
 app.use('/', require('./routes/Routes'))
 
-app.listen(3000, () =>{
+app.listen(PORT, () =>{
     console.log(port);
 })
